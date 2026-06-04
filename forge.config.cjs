df3178d98fc0608e4fca4d0195fe0122aa2e9853
Rick Peters <rick.p@socialdeal.nl>
2026-06-04 14:17:51 +0200

    fix: fix pipeline

--- forge.config.cjs
+++ forge.config.cjs
@@ -1,7 +1,7 @@
+const path = require('path');
 const { FusesPlugin } = require('@electron-forge/plugin-fuses');
 const { FuseV1Options, FuseVersion } = require('@electron/fuses');
 
-// Controleer of de build op GitHub Actions draait via de standaard CI omgevingsvariabele
 const isCI = process.env.CI === 'true';
 
 const makers = [
@@ -24,7 +24,6 @@ const makers = [
   }
 ];
 
-// Voeg de RPM-maker ALLEEN toe als de app in de GitHub Actions cloud (CI) wordt gebouwd
 if (isCI) {
   makers.push({
     name: '@electron-forge/maker-rpm',
@@ -33,11 +32,13 @@ if (isCI) {
 }
 
 module.exports = {
+  // FIX: Forceert Electron Forge om de installers ALTIJD in deze specifieke projectmap te bouwen
+  outDir: path.join(__dirname, 'dist-packages'),
   packagerConfig: {
     asar: true,
   },
   rebuildConfig: {},
-  makers: makers, // Gebruik de dynamisch opgebouwde lijst met makers
+  makers: makers,
   plugins: [
     {
       name: '@electron-forge/plugin-auto-unpack-natives',
